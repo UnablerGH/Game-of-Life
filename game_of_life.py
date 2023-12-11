6e51ccac97b042eead8f5a2e2801c83b35436d2f
Bartosz <101946927+UnablerGH@users.noreply.github.com>
2023-12-11 10:07:45 +0100

    optimization of updating grid

--- game_of_life.py
+++ game_of_life.py
@@ -1,63 +1,39 @@
 import numpy as np
-from collections import defaultdict
 import time
 import matplotlib.pyplot as plt
 
 def start_grid(size):
-    return np.random.randint(2, size=(size, size))
+    return np.random.randint(2, size=(size, size), dtype=int)
 
 def update_grid(grid):
-    new_grid = np.zeros(grid.shape, dtype=int)
-    for i in range(grid.shape[0]):
-        for j in range(grid.shape[1]):
-            new_grid[i][j] = int(update_cell(grid, i, j))
-    return new_grid
+    neighbor_count = (
+        np.roll(grid, shift=(1, 1), axis=(0, 1)) + np.roll(grid, shift=1, axis=0) +
+        np.roll(grid, shift=(1, -1), axis=(0, 1)) + np.roll(grid, shift=-1, axis=0) +
+        np.roll(grid, shift=(1, 0), axis=(0, 1)) + np.roll(grid, shift=-1, axis=1) +
+        np.roll(grid, shift=(0, 1), axis=(0, 1)) + np.roll(grid, shift=1, axis=1)
+    )
 
-def update_cell(grid, i, j):
-    neighbors = get_neighbors(grid, i, j)
-    if grid[i][j] == 1:
-        if neighbors < 2:
-            return 0
-        elif neighbors > 3:
-            return 0
-        else:
-            return 1
-    else:
-        if neighbors == 3:
-            return 1
-        else:
-            return 0
-
-def get_neighbors(grid, i, j):
-    neighbors = 0
-    for x in range(i-1, i+2):
-        for y in range(j-1, j+2):
-            if x == i and y == j:
-                continue
-            if x < 0 or y < 0 or x >= grid.shape[0] or y >= grid.shape[1]:
-                continue
-            if grid[x][y] == 1:
-                neighbors += 1
-    return neighbors
+    new_grid = ((grid == 1) & ((neighbor_count == 2) | (neighbor_count == 3))) | ((grid == 0) & (neighbor_count == 3))
+    return new_grid.astype(int)
 
 def show_grid(grid):
     plt.imshow(grid, cmap='gray')
-    plt.pause(0.1) 
+    plt.pause(0.01)
 
 def main():
-    size = 100
+    size = 30
     start = start_grid(size)
     
-    plt.ion() 
+    plt.ion()
     
-    for i in range(1000):
+    for _ in range(1000):
         if np.sum(start) == 0:
             break
         start = update_grid(start)
         show_grid(start)
         time.sleep(0.01)
 
-    plt.ioff()  
-    plt.show()  
+    plt.ioff()
+    plt.show()
 
 main()
